f398f936becb7e4a20572fd5799717c33890e4b5
TooniOlaniyan <asalamlatif@gmail.com>
2022-08-26 21:02:59 +0100

    setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Content from '../component/Content'
+import SideBar from '../component/SideBar'
+
+function Home() {
+  return (
+    <div>
+        <SideBar/>
+        <Content/>
+    </div>
+  )
+}
+
+export default Home
